b4d2163bc38e7e54d9b3a32fdb136347729b7012
shenbaoblog <shenbaoblog@gmail.com>
2024-03-01 08:43:13 +0900

    サイドバー作成

--- client/src/pages/search.jsx
+++ client/src/pages/search.jsx
@@ -1,6 +1,10 @@
+import AppLayout from '@/components/Layouts/AppLayout'
+import Layout from '@/components/Layouts/Layout'
+import Sidebar from '@/components/Sidebar'
 import axios from 'axios'
+import Head from 'next/head'
 import { useRouter } from 'next/router'
-import React, { use, useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const search = () => {
     const [results, setResults] = useState([])
@@ -29,7 +33,19 @@ const search = () => {
         }
         fetchMedia()
     }, [searchQuery])
-    return <div>search</div>
+    return (
+        <AppLayout
+            header={
+                <h2 className="font-semibold text-xl text-gray-800 leading-tight">
+                    Search
+                </h2>
+            }>
+            <Head>
+                <title>Laravel - Search</title>
+            </Head>
+            <Layout sidebar={<Sidebar />}>コンテンツ</Layout>
+        </AppLayout>
+    )
 }
 
 export default search
