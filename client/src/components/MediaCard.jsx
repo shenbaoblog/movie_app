96792e431bc791d605d9cbafd2e3ec6c7420348b
shenbaoblog <shenbaoblog@gmail.com>
2024-03-01 21:22:59 +0900

    検索中の場合、検索結果がなかった場合

--- client/src/components/MediaCard.jsx
+++ client/src/components/MediaCard.jsx
@@ -6,29 +6,34 @@ import {
     Grid,
     Typography,
 } from '@mui/material'
+import Link from 'next/link'
 import React from 'react'
 
 const MediaCard = ({ item }) => {
     const imagePath = item.poster_path
         ? `https://image.tmdb.org/t/p/original/${item.poster_path}`
-        : 'media_poster_img/NO IMAGE.png'
+        : 'media_poster_img/NO_IMAGE.png'
     return (
         <Grid item xs={12} sm={6} md={4} lg={3}>
             <Card>
                 <CardActionArea>
-                    <CardMedia
-                        component={'img'}
-                        sx={{ aspectRatio: '2/3' }}
-                        image={imagePath}
-                    />
-                    <CardContent>
-                        <Typography variant="h6" component={'div'} noWrap>
-                            {item.title || item.name}
-                        </Typography>
-                        <Typography variant="subtitle1" color="textSecondary">
-                            {item.release_date || item.first_air_date}
-                        </Typography>
-                    </CardContent>
+                    <Link href={`/detail/${item.media_type}/${item.id}`}>
+                        <CardMedia
+                            component={'img'}
+                            sx={{ aspectRatio: '2/3' }}
+                            image={imagePath}
+                        />
+                        <CardContent>
+                            <Typography variant="h6" component={'div'} noWrap>
+                                {item.title || item.name}
+                            </Typography>
+                            <Typography
+                                variant="subtitle1"
+                                color="textSecondary">
+                                {item.release_date || item.first_air_date}
+                            </Typography>
+                        </CardContent>
+                    </Link>
                 </CardActionArea>
             </Card>
         </Grid>
