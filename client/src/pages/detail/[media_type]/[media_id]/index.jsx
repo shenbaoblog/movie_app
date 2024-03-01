2d9dea485a0b7b6a327b679e5cd8791cd47d75b7
shenbaoblog <shenbaoblog@gmail.com>
2024-03-01 21:30:27 +0900

    詳細ページの分岐(movieの場合、tvの場合)

--- client/src/pages/detail/[media_type]/[media_id]/index.jsx
+++ client/src/pages/detail/[media_type]/[media_id]/index.jsx
@@ -4,7 +4,7 @@ import axios from 'axios'
 import Head from 'next/head'
 import React from 'react'
 
-const Detail = ({ detail }) => {
+const Detail = ({ detail, media_type, media_id }) => {
     console.log(detail)
     return (
         <AppLayout
@@ -65,11 +65,13 @@ const Detail = ({ detail }) => {
                         </Grid>
                         <Grid item md={8}>
                             <Typography variant="h4" paragraph>
-                                {detail.title}
+                                {detail.title || detail.name}
                             </Typography>
                             <Typography paragraph>{detail.overview}</Typography>
                             <Typography variant="h6">
-                                公開日:{detail.release_date}
+                                {media_type == 'movie'
+                                    ? `公開日:${detail.release_date}`
+                                    : `初回放送日:${detail.first_air_date}`}
                             </Typography>
                         </Grid>
                     </Grid>
@@ -99,7 +101,7 @@ export async function getServerSideProps(context) {
         const fetchData = combinedData
 
         return {
-            props: { detail: fetchData },
+            props: { detail: fetchData, media_type, media_id },
         }
     } catch (err) {
         console.log(err)
